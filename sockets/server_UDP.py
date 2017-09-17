d1e0a1679a9f62a5b833b080e49baf43c8959f12
Stephen Longofono <Onofognol@ku.edu>
2017-09-17 10:47:05 -0500

    Update server_UDP.py

--- sockets/server_UDP.py
+++ sockets/server_UDP.py
@@ -7,15 +7,12 @@ characters.
 
 Usage:
 
-    python server_UDP -A <IP Address of this server> -P <Port to listen on>
+    python server_UDP -P <Port to listen on>
     
 """
 
 import socket
-import time
 import argparse
-
-BUFLEN = 2048
 
 # Converts string argument to their uppercase representation
 def convert(myStr):
@@ -23,35 +20,48 @@ def convert(myStr):
         return str(myStr).upper()
     return myStr.upper()
 
+# Per instructions, "use a function to print address and port"
+def print_address_port(a,b):
+    print("\tIP Address {}\n\tPort {}\n".format(a,b))
+
+BUFLEN = 2048
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--port', '-P', type=int, help='The desired port to listen on', required=True)
-parser.add_argument('--address', '-A', type=str, help='The IP address of the remote server to connect to', required=True)
 
 args = parser.parse_args()
 
 PORT = args.port
-IP_ADDR = args.address
-
 
 try:
     # Set up connection, use internet protocol and datagrams (UDP)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((IP_ADDR, PORT))
+    sock.bind(('', PORT))
+    print("Local socket created")
+    print("Press Ctrl-C to quit")
+
 
     while True:
         # Grab up to BUFLEN bytes from the connection
         payload, address = sock.recvfrom(BUFLEN)
-        print("Received {} from host {}...".format(payload, address))
 
         if payload:
+            print("Local details:\n")
+            localaddr, localport = sock.getsockname()
+            print_address_port(localaddr, localport)
+
+            print("Received {} from a client...".format(payload)
+            print("Remote details:\n")
+            print_address_port(address[0], address[1])
+
             # Send back the altered text
             # We can't use the same port we are listening on, since it will
             # create an endless loop.  Instead, we simply send it to the
             # address we received the message from, and let the other end
             # decide what to do with it.
             sock.sendto(convert(payload), address)
-        time.sleep(0.5)
 
 except KeyboardInterrupt:
     # Handle intentional quit gracefully
     print("\n\nQuitting...")
+
