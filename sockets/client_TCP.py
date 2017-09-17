93cfdbb025f96fe0908ee013e773273862dca243
Stephen Longofono <Onofognol@ku.edu>
2017-09-17 11:13:25 -0500

    Update client_TCP.py

--- sockets/client_TCP.py
+++ sockets/client_TCP.py
@@ -1,3 +1,15 @@
+"""
+client_TCP.py
+
+This file demonstrates a simple TCP client which sends data to a remote server
+and gathers the reply.
+
+Usage:
+
+    python client_TCP.py -A <IP Address of the remote server> -P <Desired port on the remote server>
+    
+"""
+
 import socket
 import argparse
 
@@ -7,6 +19,10 @@ def trim(myStr):
         print("Your input was larger than the maximum of {} bytes and has been truncated to fit.".format(BUFLEN))
         return myStr[:2048-len(myStr)]
     return myStr
+
+# Per instructions, "print the address and port using a function"
+def print_address_port(a,b):
+    print("\tIP Address {}\n\tPort {}\n".format(a,b))
 
 BUFLEN = 2048
 
@@ -19,7 +35,6 @@ args = parser.parse_args()
 PORT = args.port
 SERVER_ADDR = args.address
 
-print("The port passed in was {}".format(PORT))
 print("Attempting to connect to IP address {} on port {}...".format(SERVER_ADDR, PORT))
 
 try:
@@ -36,11 +51,18 @@ try:
 
         # Send the user's input through the socket TCP connection
         sock.send(trim(raw))
-        
+
+        print("Local details:\n")
+        localaddress, localport = sock.getsockname()
+        print_address_port(localaddress, localport)
+
         # Grab up to BUFLEN bytes from the connection reply
         payload = sock.recv(BUFLEN)
         if payload:
             print("Return text from the server: {}".format(payload))
+            print("Remote details:\n")
+            print_address_port(SERVER_ADDR, PORT)
+            
 
 except KeyboardInterrupt:
     # Handle intentional quit gracefully
@@ -48,5 +70,5 @@ except KeyboardInterrupt:
 
 finally:
     # Tear-down connection
-    if None != sock:
+    if sock is not None:
         sock.close()
